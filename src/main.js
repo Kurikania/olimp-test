99e5ac3658dc63a05724f6cfd900028d50fb9a31
Kurikania <65830166+Kurikania@users.noreply.github.com>
2021-04-01 19:26:39 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueTour from 'vue-tour'
+import vueEditor from 'vue2-editor'
+import VueScrollTo from 'vue-scrollto'
+
+require('vue-tour/dist/vue-tour.css')
+
+Vue.use(VueTour);
+Vue.use(VueScrollTo, {
+  container: "#scroller",
+})
+Vue.use(vueEditor);
+
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,139 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+<div>
+<div class="workbook">
+  <div class="tree" >
+       <constructionTree />
   </div>
+  <div class="main">
+    <div class="project-filters" style="display: flex;">
+      <div class="filters">
+        <span> Записи </span>
+        <span> Статьи </span>
+        <span> О проекте </span>
+        </div>
+        <div style="display: flex; align-items: center;">
+        <select name="filters">
+        <option value="all">Фильтры</option>
+        <option value="idk">Я не знаю</option>
+        </select>
+        <input type="text" value="">
+      </div>
+    </div>
+    <div class="flex-column" id="scroller" style="overflow: auto; height: 80vh">
+      <div class="post flex-column" v-for="(post, index) in posts" :key="index" :id="post.post_id">
+        <div class="post-header">
+        <span> {{ post.section }} </span>
+        <span> {{ post.branch }} </span>
+        <span> {{post.date}} </span>
+        </div>
+        <div v-html='post.content'> </div>
+        <div class="post-footer">
+          <span>{{post.author}}</span>
+        </div>
+        </div>
+        <button>Добавить запись </button>
+        <div>
+        <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+/* eslint-disable */
+import axios from "axios"
+// import { mapGetters } from "vuex";
+import { VueEditor } from "vue2-editor";
+import constructionTree from "../components/construcrionTree";
+import managTree from "../components/managTree";
 
 export default {
-  name: 'Home',
+  layout: "olimp",
+  data() {
+    return {
+      content: "<p>Some initial content</p>",
+      customToolbar: [
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["image", "code-block"],
+      ],
+      posts: []
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    VueEditor,
+    constructionTree,
+    managTree
+  },
+   methods: {
+      async load() {
+        const res = await axios.get("/data.json"); // dev
+        this.posts = res.data;
+      },
+      treeClick(id) {
+        console.log(id)
+      }
+   },
+  async mounted() {
+    this.load()
+
+  },
+};
 </script>
+
+<style lang="css" scoped>
+.workbook {
+  display: flex;
+  padding: 10px;
+}
+.tree {
+  background-color: rgb(240, 240, 240);
+  width: 30%;
+  height: 90vh;
+  overflow: auto;
+}
+.main {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+}
+.post {
+  border: 1px solid #ccccff;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 7px 30px;
+}
+.post-header {
+  font-family: Roboto;
+  font-size: 12px;
+  color: #3914af;
+  line-height: 14px;
+}
+
+.post-body {
+  font-family: Roboto;
+  font-size: 12px;
+  color: #3914af;
+  line-height: 14px;
+}
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+::-webkit-scrollbar-track {
+  background: #b1cfd6;
+  border-left: 1px solid #a1bdf3;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #bcd1e9;
+  border: solid 3px #98c0fd;
+  border-radius: 7px;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: white;
+}
+</style>
